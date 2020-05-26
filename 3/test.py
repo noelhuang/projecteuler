61ba8d8a47e0ce29a32c949b6d78ba455c95136d
prettyflyforanoobguy <noeller218@hotmail.com>
2020-05-26 17:44:32 +0200

    004

--- 3/test.py
+++ 3/test.py
@@ -1,32 +1,24 @@
 primetest = 2
-primeflag = False
 bignumber = int(input("Pleasy enter a big number (smaller than yo momma)"))
 print("   Bignumber = " + str(bignumber))
 biggestprimefactor = 0
-while bignumber != 1:
-    primetest += 1
+while bignumber > 2:
 
+    primeflag = True
     for x in range(2, primetest):
         if primetest % x == 0:
             primeflag = False
             break
-        else:
-            primeflag = True
 
     if primeflag:
-        #print(str(primetest) + " is a prime number")
+        # print(str(primetest) + " is a prime number")
         if bignumber % primetest == 0:
             bignumber = bignumber / primetest
             print("newBignumber = " + str(bignumber))
             if primetest > biggestprimefactor:
                 biggestprimefactor = primetest
-            primetest = 2
-            continue
-        else:
-            continue
+            primetest = 1
 
-    else:
-        #print(str(primetest) + " is not a prime number")
-        primeflag = False
+    primetest += 1
 
 print("This is the biggest prime factor: " + str(biggestprimefactor))
