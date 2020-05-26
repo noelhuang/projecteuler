39a116740440b8df2aedf93d53491ddd0eab0941
prettyflyforanoobguy <noeller218@hotmail.com>
2020-05-26 19:07:26 +0200

    Merge conflicts

--- 5/005.py
+++ 5/005.py
@@ -1,34 +1,14 @@
-def method1():
-    divflag = False
+divflag = False
 
-    for x in range(10000000):
-        for y in range(1,11):
-            if x % y == 0:
-                divflag = True
-            else:
-                divflag = False
-                break
+for x in range(1000000000):
+    for y in range(1, 21):
+        if x % y == 0:
+            divflag = True
+        else:
+            divflag = False
+            break
 
-        if divflag:
-            if x != 0:
-                print(x)
-                break
-
-
-def method2(x):
-    found = False
-    i = 1
-    while not found:
-        if getFlag(i, x):
-            return i
-        i += 1
-
-
-def getFlag(i, x):
-    for j in range(1, x):
-        if i % j != 0:
-            return False
-    return True
-
-
-print(print(method2(21)))
+    if divflag:
+        if x != 0:
+            print(x)
+            break
