b32558832c8536b62eb2f243d3ffd95a56c8e28b
prettyflyforanoobguy <noeller218@hotmail.com>
2021-11-28 13:58:39 +0100

    015 slow

---
+++ 015/015.py
@@ -0,0 +1,30 @@
+from random import random
+
+temppath = []
+
+paths = []
+
+gridsize = 2
+
+for j in range(1, 1000):
+    for i in range(1, (2*gridsize)+1):
+        num = random()
+        if num > 0.5:
+            if temppath.count('r') >= gridsize:
+                temppath.append('d')
+            else:
+                temppath.append('r')
+
+        else:
+            if temppath.count('d') >= gridsize:
+                temppath.append('r')
+            else:
+                temppath.append('d')
+    temptuple = tuple(temppath)
+    paths.append(temptuple)
+    temppath = []
+
+print(paths)
+print(f"{len(paths)} is path number with duplicates")
+nodupepaths = set(paths)
+print(f"{len(nodupepaths)} is path number with no duplicates")
